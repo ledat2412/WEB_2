e90aed2f8367009ae9c0923f65655f565a5229b6
Huy <lyminhhuy01234@gmail.com>
2025-04-22 15:45:45 +0700

    xoa

--- app/view/admin/js/Click.js
+++
@@ -1,7 +0,0 @@
-const buttonClick = document.querySelector('.Button'); 
-const buttonInfo = document.querySelector('.button-infor');
-
-buttonClick.addEventListener('click', function () {
-    buttonInfo.classList.toggle('visible');
-});
-
